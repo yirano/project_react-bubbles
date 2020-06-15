ffabf10515f1dc3dca919b7207d5df940bb22a0e
yirano <yirano@protonmail.ch>
2020-06-14 21:27:01 -0400

    Set up building blocks for user authentication - axiosWithAuth() - logIn() - PrivateRoute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react"
+import { axiosWithAuth } from './axiosWithAuth'
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [cred, setCred] = useState({})
+
+  const login = e => {
+    e.preventDefault()
+    axiosWithAuth()
+      .post('/login', cred)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubbles')
+      })
+      .catch(err => console.log('Error logging in: ', err))
+  }
+
+  const handleChange = e => {
+    setCred({ ...cred, [e.target.name]: e.target.value })
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+        <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+        <input type="submit" value="Log In" />
+      </form>
     </>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
